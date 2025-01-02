17545d63e7abd77fd438a7aac59ec40180d0b330
mconley91 <matthew.e.conley@gmail.com>
2025-01-02 17:24:23 -0500

    add ciphering functionality and out of bounds handling

--- cipher.rb
+++ cipher.rb
@@ -1,7 +1,30 @@
+
 puts "Enter a word to be encrypted: " 
 word = gets.chomp
-puts "Now enter a number to be encrypted: "
+puts "Now enter a number to shift your letters by: "
 number = gets.chomp.to_i
 
+def wrap_26(num)
+  if num > 26
+    loop do
+      num -= 26
+      if num - 26 < 0
+        return num
+      end
+    end
+  end
+  return num
+end
 
-puts "Output: #{word} & #{number}"
+def caesar_cipher(str,num)
+  alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  
+  caesared_word = str.split("").map{|letter| letter == letter.upcase ? 
+  alphabet[wrap_26(alphabet.index(letter.downcase) + num)].upcase : 
+  alphabet[wrap_26(alphabet.index(letter.downcase) + num)]}.join("")
+
+  puts "Output: #{caesared_word} Number: #{num}"
+end
+
+caesar_cipher(word,number)
+
