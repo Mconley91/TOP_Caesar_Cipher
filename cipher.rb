688e52213b80d97591fa773d7faa055e4cf00a0d
mconley91 <matthew.e.conley@gmail.com>
2025-01-02 17:37:56 -0500

    add valid letter checker

--- cipher.rb
+++ cipher.rb
@@ -16,14 +16,22 @@ def wrap_26(num)
   return num
 end
 
+def check_str(str,alphabet)
+  str.split("").all?{|letter| alphabet.include?(letter)} ? true : false
+end
+
 def caesar_cipher(str,num)
   alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  
+  if !check_str(str,alphabet)
+    puts "Invalid Entry"
+    return
+  end
+
   caesared_word = str.split("").map{|letter| letter == letter.upcase ? 
   alphabet[wrap_26(alphabet.index(letter.downcase) + num)].upcase : 
   alphabet[wrap_26(alphabet.index(letter.downcase) + num)]}.join("")
 
-  puts "Output: #{caesared_word} Number: #{num}"
+  puts "Output: #{caesared_word}"
 end
 
 caesar_cipher(word,number)
