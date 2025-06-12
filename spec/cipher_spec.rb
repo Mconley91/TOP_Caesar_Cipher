ac24d76877793aeeb7d0b96cd4c718ae401cad3c
mconley91 <matthew.e.conley@gmail.com>
2025-06-12 13:46:11 -0400

    add tests for #check_str and #check_num

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -14,3 +14,39 @@ describe '#wrap26' do
     end
   end
 end
+
+describe '#check_str' do
+  context 'checks all characters in input and returns a boolean' do
+    it 'returns false if input contains numbers' do
+      alphabet = %w[a b c d e f g h i j k l m n o p q r s t u
+                    v w x y z]
+      input = 'warhammer40k'
+      expect(check_str(input, alphabet)).to be false
+    end
+    it 'returns true when only letters are used' do
+      alphabet = %w[a b c d e f g h i j k l m n o p q r s t u
+                    v w x y z]
+      input = 'warhammer'
+      expect(check_str(input, alphabet)).to be true
+    end
+    it 'returns true when spaces and exclamation points are used' do
+      alphabet = %w[a b c d e f g h i j k l m n o p q r s t u
+                    v w x y z]
+      input = '                 !!!!'
+      expect(check_str(input, alphabet)).to be true
+    end
+  end
+end
+
+describe '#check_num' do
+  context 'checks that num is not zero' do
+    it 'returns true if num is not zero' do
+      num = 1
+      expect(check_num(num)).to be true
+    end
+    it 'returns false if num is zero' do
+      num = 0
+      expect(check_num(num)).to be false
+    end
+  end
+end
